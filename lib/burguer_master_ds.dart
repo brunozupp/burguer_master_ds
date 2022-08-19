53048ec6984236263e3daa79098f03c665409aba
Bruno Noveli <brunimzuppwolf@gmail.com>
2022-08-18 22:24:11 -0300

    Adicionado text field

--- lib/burguer_master_ds.dart
+++ lib/burguer_master_ds.dart
@@ -1,6 +1,7 @@
 library burguer_master_ds;
 
 export 'src/components/burguer_master_button.dart';
+export 'src/components/burguer_master_text_field.dart';
 
 export 'src/styles/measurements.dart';
 export 'src/styles/text_styles.dart';
---
+++ lib/src/components/burguer_master_text_field.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class BurguerMasterTextField extends StatelessWidget {
+
+  const BurguerMasterTextField({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
