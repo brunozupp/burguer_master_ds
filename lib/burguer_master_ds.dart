f9424d7f291710f03227f291b225766ed9fcde3d
Bruno Noveli <brunimzuppwolf@gmail.com>
2022-08-18 22:32:59 -0300

    Adicionado select field

--- lib/burguer_master_ds.dart
+++ lib/burguer_master_ds.dart
@@ -2,6 +2,7 @@ library burguer_master_ds;
 
 export 'src/components/burguer_master_button.dart';
 export 'src/components/burguer_master_text_field.dart';
+export 'src/components/burguer_master_select_field.dart';
 
 export 'src/styles/measurements.dart';
 export 'src/styles/text_styles.dart';
---
+++ lib/src/components/burguer_master_select_field.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class BurguerMasterSelectField extends StatelessWidget {
+
+  const BurguerMasterSelectField({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
